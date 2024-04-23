186cfa68868d9027d812150f440a266ba9bb3c94
ParaQueNome <guiagusto455@gmail.com>
2024-04-23 12:05:11 -0300

    OpenAPI Service

--- Codificacao/app/services/opeaiApiService/openaiApi.py
+++ Codificacao/app/services/opeaiApiService/openaiApi.py
@@ -7,12 +7,5 @@ class OpenAiClient:
         load_dotenv()
         self.openai = OpenAI(api_key= os.getenv("OPENAI_API_KEY"))
 
-    def get_response(self):
-        completion = self.openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-                {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-            ]
-        )
-        print(completion.choices[0].message.content)
+   def userFinances(self, **renda, **despesa):
+        
