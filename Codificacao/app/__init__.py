9abc5d7b6107eca00a69c9958ab3acf9487c0776
ParaQueNome <guiaugusto455@gmail.com>
2024-04-29 00:19:25 -0300

    Adicionando modal de cadastro dos gastos na planilha de finanças adicionando flask-bootstrap para controle do front-end Modificando template base.html com estilizacao Criacao da pasta static/css para modelos customizados

--- Codificacao/app/__init__.py
+++ Codificacao/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
 from app.controller.authControl import auth_bp
 from app.controller.financeControl import financeBp
@@ -7,7 +8,7 @@ from app.services.opeaiApiService.openaiApi import OpenAiClient
 def create_app():
     # Criando instancia da aplicação Flask com o nome da aplicação
     app = Flask(__name__, template_folder='templates')
-
+    bootstrap = Bootstrap5(app)
     # Importando as rotas da aplicação
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(financeBp, url_prefix='/finance')
@@ -16,5 +17,4 @@ def create_app():
     app.config['SECRET_KEY'] = 't%23s342%%@'
     
     return app
-
 
